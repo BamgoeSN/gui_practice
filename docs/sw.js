feff6bd08f91fc937a36755ae894804b27cb7d5d
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2022-04-06 10:11:14 +0900

    Initial commit

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './gui_practice.js',
+  './gui_practice_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
